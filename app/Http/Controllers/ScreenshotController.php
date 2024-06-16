9a725ff1f5f9119a5222758a09e86101ca2e8467
Dylan Duault <dduault@bynativ.com>
2024-06-16 19:46:11 +0200

    feat: Create screenshot model to have an history and log IPs

--- app/Http/Controllers/ScreenshotController.php
+++ app/Http/Controllers/ScreenshotController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TakeScreenshotResource;
+use App\Models\Screenshot;
 use Illuminate\Http\Request;
 
 class ScreenshotController extends Controller
@@ -12,24 +14,23 @@ class ScreenshotController extends Controller
      * Website main functionality, take a screenshot of a given URL and return the image URL
      *
      * @param  Request  $request
-     * @return array
+     * @return TakeScreenshotResource
      */
     public function takeScreenshot(Request $request)
     {
         $url = $request->input('url');
-        $filename = md5($url) . '.png';
+        $md5 = md5($url);
+        $filename = $md5 . '.png';
         $path = public_path('screenshots/' . $filename);
         $apikey = env('API_FLASH_KEY');
         $command = "../screenshot.sh $url $path $apikey";
         exec($command);
 
-        return [
-            'url' => $this->getImageUrl($filename)
-        ];
-    }
+        $screenshot = Screenshot::create([
+            'md5' => $md5,
+            'ip' => $request->ip(),
+        ]);
 
-    private function getImageUrl($filename)
-    {
-        return url('screenshots/' . $filename);
+        return new TakeScreenshotResource($screenshot);
     }
 }
---
+++ app/Http/Resources/TakeScreenshotResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TakeScreenshotResource extends JsonResource
+{
+
+    public static $wrap = null;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'image_url' => $this->getImageUrl($this->md5),
+        ];
+    }
+
+    private function getImageUrl(string $md5): string
+    {
+        return url('screenshots/' . $md5 . '.png');
+    }
+}
