8eb07f59fc7d7a1b4845d7dfe0906c8b66b594d5
Dylan Duault <dduault@bynativ.com>
2024-06-16 19:01:51 +0200

    refactor(ScreenShotController): commentaire supprimé

--- app/Http/Controllers/ScreenshotController.php
+++ app/Http/Controllers/ScreenshotController.php
@@ -8,9 +8,6 @@ class ScreenshotController extends Controller
 {
     public function takeScreenshot(Request $request)
     {
-//        return [
-//            'image_url' => 'http://127.0.0.1:8000/screenshots/0edf27c83d4aa4699c0625d27be0e371.png'
-//        ];
 
         $url = $request->input('url');
         $filename = md5($url) . '.png';
