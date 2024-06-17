027ea8c9d7b715ee302ca93f71aaa7b269cacc9f
Dylan Duault <dduault@bynativ.com>
2024-06-17 12:25:38 +0200

    refactor: add name to homepage route

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,7 @@ use \App\Http\Controllers\ScreenshotController;
 
 Route::get('/', function () {
     return view('homepage');
-});
+})->name('homepage');
 
 Route::get('/screenshots/', [ScreenshotController::class, 'index'])->name('screenshot.index');
-Route::get('/screenshots/{md5}', [ScreenshotController::class, 'show'])->name('screenshot.show');
 Route::post('/screenshots/take', [ScreenshotController::class, 'takeScreenshot'])->name('screenshot.take');
